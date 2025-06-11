6198801aa29109619a8d8f0f36c1f96a372c1206
injin0301 <injinkim0808@gmail.com>
2025-06-11 17:57:15 +0200

    cart entity

--- cart-service/src/cart/entities/cart.entity.ts
+++ cart-service/src/cart/entities/cart.entity.ts
@@ -1,1 +1,14 @@
-export class Cart {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Cart {
+    
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+}
